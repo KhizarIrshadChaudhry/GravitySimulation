8636a6b2bea50ca442d360acf3ecc0e33b6c51d8
Khizar Irshad Chaudhry <k.irshadch@gmail.com>
2025-03-21 17:49:06 +0100

    Tilføjet kraft mellem legemer

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let legemer = []; //liste til alle legemer
 let gitterStr = 200;
+let G = 1;
 
 function setup(){
     createCanvas(windowWidth, windowHeight, WEBGL);
@@ -17,6 +18,7 @@ function draw(){
     orbitControl();
 
     
+    
     for (let z = -gitterStr; z<gitterStr; z+=20){
         beginShape();
             vertex(-200, 0, z);
@@ -32,25 +34,59 @@ function draw(){
     }
 
     
+
+    
     for (let legeme of legemer){
-        fill(legeme.farve);
-        translate(legeme.position.x, -20, legeme.position.y)
-        sphere(20);
+        push(); //push & pop da translate() tilføjer på sidste translate()
+            //noStroke();
+            fill(legeme.farve);
+            translate(legeme.position.x, -20, legeme.position.y)
+            sphere(20);
+        pop();
+        legeme.opdatere();
     }
 
 }
+
+
 
 class Legeme {
     // pos, startHastighed er 2D vector og farve er en liste
     constructor(pos, masse, startHastighed, farve=[255, 255, 255]){
         this.position = pos.copy();
         this.masse = masse;
-        this.startHastighed = startHastighed.copy()
+        this.hastighed = startHastighed.copy()
         this.acceleration = createVector(0, 0)
         this.farve = farve;
     }
 
     create(){
         legemer.push(this);
+    }
+
+    opdatere(){
+        let dt = 1; //tidsintervalet delta t 
+
+        this.acceleration = createVector(0, 0);
+
+        for (let andre of legemer){
+            if (this !== andre){
+                let kraftensRetning = p5.Vector.sub(andre.position, this.position);
+
+                let afstandSq = kraftensRetning.magSq(); //afstanden i anden
+                let afstand = sqrt(afstandSq);
+
+                let kraftensStr = (G*this.masse*andre.masse)/(afstandSq); //newtowns formel for kraft
+
+                kraftensRetning.normalize();
+                
+                let kraften = kraftensRetning.mult(kraftensStr); //bestem kraft vektoren ved at gange kraftensretning vec med dets str
+
+                this.acceleration.add(p5.Vector.div(kraften, this.masse)) //F=ma, derfor a=F/m
+            }
+        }
+        
+        this.hastighed.add(p5.Vector.mult(this.acceleration, dt))
+        this.position.add(p5.Vector.mult(this.hastighed, dt))
     }
 }
