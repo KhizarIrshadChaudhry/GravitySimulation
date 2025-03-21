77e49d55d8f6f681d7ede816c0e5e0d58e749c57
Khizar Irshad Chaudhry <k.irshadch@gmail.com>
2025-03-21 14:42:48 +0100

    Tilføjet sphere class

--- main.js
+++ main.js
@@ -8,5 +8,22 @@ function setup(){
 function draw(){
     background(200);
     orbitControl()
-    sphere(100, 10, 20)
+
+    new Legeme(createVector(0, 0), 20, createVector(0, 0), [255, 0, 0]).create();
+
+}
+
+class Legeme {
+    // pos, startHastighed er 2D vector og farve er en liste
+    constructor(pos, masse, startHastighed, farve=[255, 255, 255]){
+        this.position = pos.copy();
+        this.masse = masse;
+        this.startHastighed = startHastighed.copy()
+        this.farve = farve;
+    }
+
+    create(){
+        fill(this.farve);
+        sphere(this.masse);
+    }
 }
