20d6416578c5bbe00da10d04193a7dca93b0600e
Khizar Irshad Chaudhry <k.irshadch@gmail.com>
2025-03-21 19:27:49 +0100

    Ændret kode opstilling og gjort det mere læselig

--- main.js
+++ main.js
@@ -1,92 +1,69 @@
-let legemer = []; //liste til alle legemer
-let gitterStr = 200;
-let G = 1;
+let legemer = []; // liste til alle legemer
+let G = 1; // Gravitationens konstant
 
-function setup(){
-    createCanvas(windowWidth, windowHeight, WEBGL);
-    background(200);
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
 
-    new Legeme(createVector(0, 0), 20, createVector(0, 0), [255, 0, 0]).create();
+  legemer.push(new Legeme(createVector(0, 0), 20, createVector(0, 0)))
 
-    new Legeme(createVector(200, 100), 20, createVector(0, 0), [0, 255, 0]).create();
-    
-
+  legemer.push(new Legeme(createVector(200, 0), 20, createVector(0, 0)))
 }
 
-function draw(){
-    background(200);
-    orbitControl();
+function draw() {
+  background(0);
+  orbitControl();
+  rotateX(1);
 
-    
-    
-    for (let z = -gitterStr; z<gitterStr; z+=20){
-        beginShape();
-            vertex(-200, 0, z);
-            vertex(200, 0, z);
-        endShape();
-    }
+  //opdatere hvert legeme
+  for (let legeme of legemer) {
+    legeme.opdatere(legemer);
+  }
+  
 
-    for (let x = -gitterStr; x<gitterStr; x+=20){
-        beginShape();
-            vertex(x, 0, -200);
-            vertex(x, 0, 200);
-        endShape();
-    }
-
-    
-
-    
-    for (let legeme of legemer){
-        push(); //push & pop da translate() tilføjer på sidste translate()
-            //noStroke();
-            fill(legeme.farve);
-            translate(legeme.position.x, -20, legeme.position.y)
-            sphere(20);
-        pop();
-        legeme.opdatere();
-    }
-
+  
+  // tegb legemerne
+  noStroke();
+  for (let m of legemer) {
+    push();
+      fill(m.farve);
+      translate(m.position.x, m.position.y, 0);
+      sphere(10);
+    pop();
+  }
 }
-
-
 
 class Legeme {
-    // pos, startHastighed er 2D vector og farve er en liste
-    constructor(pos, masse, startHastighed, farve=[255, 255, 255]){
-        this.position = pos.copy();
-        this.masse = masse;
-        this.hastighed = startHastighed.copy()
-        this.acceleration = createVector(0, 0)
-        this.farve = farve;
+  // position og startHastighed er 2D-vektorer, og farve er en liste
+  constructor(pos, masse, startHastighed, farve = [255, 0, 0]) {
+    this.position = pos.copy();
+    this.hastighed = startHastighed.copy();
+    this.acceleration = createVector(0, 0);
+    this.masse = masse;
+    this.farve = farve;
+  }
+
+
+  opdatere(alleLegemer) {
+    let dt = 0.5; // tidsintervallet delta t
+    this.acceleration = createVector(0, 0);
+
+    for (let andre of alleLegemer) {
+      if (this !== andre) {
+
+        let kraftRetning = p5.Vector.sub(andre.position, this.position);
+        let afstandSq = kraftRetning.magSq(); // afstanden i anden
+
+        let kraftStr = (G * this.masse * andre.masse) / (afstandSq);
+
+        kraftRetning.normalize();
+        let kraft = p5.Vector.mult(kraftRetning, kraftStr);
+        // Ifølge F = m * a  =>  a = F/m
+        this.acceleration.add(p5.Vector.div(kraft, this.masse));
+      }
     }
 
-    create(){
-        legemer.push(this);
-    }
-
-    opdatere(){
-        let dt = 1; //tidsintervalet delta t 
-
-        this.acceleration = createVector(0, 0);
-
-        for (let andre of legemer){
-            if (this !== andre){
-                let kraftensRetning = p5.Vector.sub(andre.position, this.position);
-
-                let afstandSq = kraftensRetning.magSq(); //afstanden i anden
-                let afstand = sqrt(afstandSq);
-
-                let kraftensStr = (G*this.masse*andre.masse)/(afstandSq); //newtowns formel for kraft
-
-                kraftensRetning.normalize();
-                
-                let kraften = kraftensRetning.mult(kraftensStr); //bestem kraft vektoren ved at gange kraftensretning vec med dets str
-
-                this.acceleration.add(p5.Vector.div(kraften, this.masse)) //F=ma, derfor a=F/m
-            }
-        }
-        
-        this.hastighed.add(p5.Vector.mult(this.acceleration, dt))
-        this.position.add(p5.Vector.mult(this.hastighed, dt))
-    }
+    // Opdater  hastiguhen og position
+    this.hastighed.add(p5.Vector.mult(this.acceleration, dt));
+    this.position.add(p5.Vector.mult(this.hastighed, dt));
+  }
 }
