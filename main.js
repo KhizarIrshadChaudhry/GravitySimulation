79b6676632134b35d211dc1271a3885a8d30bcf5
Khizar Irshad Chaudhry <k.irshadch@gmail.com>
2025-03-21 17:10:16 +0100

    Tilføjet gitter og lavet liste til legemer

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
+let legemer = []; //liste til alle legemer
+let gitterStr = 200;
 
 function setup(){
     createCanvas(windowWidth, windowHeight, WEBGL);
     background(200);
 
+    new Legeme(createVector(0, 0), 20, createVector(0, 0), [255, 0, 0]).create();
+
+    new Legeme(createVector(200, 100), 20, createVector(0, 0), [0, 255, 0]).create();
+    
+
 }
 
 function draw(){
     background(200);
-    orbitControl()
+    orbitControl();
 
-    new Legeme(createVector(0, 0), 20, createVector(0, 0), [255, 0, 0]).create();
+    
+    for (let z = -gitterStr; z<gitterStr; z+=20){
+        beginShape();
+            vertex(-200, 0, z);
+            vertex(200, 0, z);
+        endShape();
+    }
+
+    for (let x = -gitterStr; x<gitterStr; x+=20){
+        beginShape();
+            vertex(x, 0, -200);
+            vertex(x, 0, 200);
+        endShape();
+    }
+
+    
+    for (let legeme of legemer){
+        fill(legeme.farve);
+        translate(legeme.position.x, -20, legeme.position.y)
+        sphere(20);
+    }
 
 }
 
@@ -19,11 +46,11 @@ class Legeme {
         this.position = pos.copy();
         this.masse = masse;
         this.startHastighed = startHastighed.copy()
+        this.acceleration = createVector(0, 0)
         this.farve = farve;
     }
 
     create(){
-        fill(this.farve);
-        sphere(this.masse);
+        legemer.push(this);
     }
 }
